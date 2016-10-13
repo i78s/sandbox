f2321fa1f9976d04faa38b762f958e77ee9708d0
稲葉 聡 <inaba182@gmail.com>
2016-10-13 23:11:09 +0900

    first commit

---
+++ react-sample/config/webpack.core.js
@@ -0,0 +1,41 @@
+'use strict';
+const core = require('./core.config');
+const webpack = require("webpack");
+const entries = require("webpack-entries");
+/**
+ * webpack config
+ * url: https://webpack.github.io/docs/configuration.html
+ */
+const webpackConfig = {
+    entry: entries('./src/js/!(_*|*spec).js',true),
+    output: {
+        path: core.basePath + '/assets/js',
+        publicPath: '/assets',
+        filename: "[name].js",
+        sourceMapFilename: 'maps/[name].map',
+        jsonpFunction: 'fr'
+    },
+    resolve: { modules: [ './src/js'] },
+    module: {
+        loaders: [
+            {test: /\.js$/, exclude: /node_modules/, loader: 'eslint', enforce: 'pre'},
+            {test: /\.html$/, loader: 'html'},
+            {test: /\.json$/, loader: 'json'},
+        ],
+        exprContextCritical: false
+    },
+    plugins: [
+        new webpack.LoaderOptionsPlugin({
+            options: {
+                eslint: {
+                    configFile: __dirname + '/../.eslintrc',
+                    failOnError: true
+                },
+                babel: {
+                    presets: ["es2015"]
+                }
+            }
+        })
+    ]
+};
+module.exports = webpackConfig;
