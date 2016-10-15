c7cee516329195021b57d26abb87ceb082633503
稲葉 聡 <inaba182@gmail.com>
2016-10-15 22:15:25 +0900

    チュートリアル1ページ目まで

--- react-sample/src/js/components/comment-box.js
+++ react-sample/src/js/components/comment-box.js
@@ -8,6 +8,8 @@ export default class CommentBox extends Component {
   constructor(props) {
     super(props);
 
+    this.handleCommentSubmit = this.handleCommentSubmit.bind(this);
+
     this.state = {
       data: []
     };
@@ -31,12 +33,22 @@ export default class CommentBox extends Component {
       });
   }
 
+  handleCommentSubmit(comment) {
+    let comments = this.state.data;
+    comment.id = Date.now();
+
+    let newComment = comments.concat([comment]);
+    this.setState({data: newComment});
+
+    // TODO: submit to the server and refresh the list
+  }
+
   render() {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
         <CommentList data={this.state.data} />
-        <CommentForm />
+        <CommentForm onCommentSubmit={this.handleCommentSubmit} />
       </div>
     );
   }
--- react-sample/src/js/components/comment-form.js
+++ react-sample/src/js/components/comment-form.js
@@ -3,13 +3,56 @@ import React, {Component} from 'react';
 export default class CommentForm extends Component {
   constructor() {
     super();
+
+    this.handleAuthorChange = this.handleAuthorChange.bind(this);
+    this.handleTextChange = this.handleTextChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {
+      author: '',
+      text: ''
+    };
+  }
+
+  handleAuthorChange(e) {
+    this.setState({
+      author: e.target.value
+    });
+  }
+
+  handleTextChange(e) {
+    this.setState({
+      text: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    let author = this.state.author.trim();
+    let text = this.state.text.trim();
+    if (!text || !author) {
+      return;
+    }
+
+    this.props.onCommentSubmit({author: author, text: text});
+    this.setState({author: '', text: ''});
   }
 
   render() {
     return (
-      <div className="commentForm">
-        Hello, world! I am a CommentForm.
-      </div>
+      <form className="commentForm" onSubmit={this.handleSubmit}>
+        <input type="text"
+               placeholder="Your name"
+               value={this.state.author}
+               onChange={this.handleAuthorChange}
+          />
+        <input type="text"
+               placeholder="Say something..."
+               value={this.state.text}
+               onChange={this.handleTextChange}
+          />
+        <input type="submit" value="Post" />
+      </form>
     );
   }
 }
