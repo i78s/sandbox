186e53c52b77ed1d60db89ce7739ca976609f425
稲葉 聡 <inaba_satoshi@liginc.co.jp>
2017-09-26 16:02:45 +0900

    sw-precacheでオフラインページのサンプルを作った

---
+++ sw-precache-sample/sw-precache-config.js
@@ -0,0 +1,14 @@
+const packageJson = require('./package.json');
+
+module.exports = {
+    swFile: 'app/service-worker.js',
+    staticFileGlobs: [
+        'app/assets/**/*.css',
+        'app/assets/**/*.js',
+        'app/assets/images/**/*'
+    ],
+    stripPrefix: 'app/',
+
+    cacheId: packageJson.name,
+    verbose: true
+};
