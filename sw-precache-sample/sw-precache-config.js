831d7c3dcdeef6d4471f102c6b084e75731d692a
稲葉 聡 <inaba_satoshi@liginc.co.jp>
2017-09-27 15:12:38 +0900

    htmlもキャッシュ対象に含めた

--- sw-precache-sample/sw-precache-config.js
+++ sw-precache-sample/sw-precache-config.js
@@ -3,6 +3,7 @@ const packageJson = require('./package.json');
 module.exports = {
     swFile: 'app/service-worker.js',
     staticFileGlobs: [
+        'app/**/*.html',
         'app/assets/**/*.css',
         'app/assets/**/*.js',
         'app/assets/images/**/*'
