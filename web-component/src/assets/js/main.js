b7be2bece744344741a06bfc90d7d2e89bf2c345
稲葉 聡 <inaba_satoshi@liginc.co.jp>
2017-11-09 16:03:39 +0900

    githubのリポジトリ一覧を取ってくるコンポーネントを作ってみた

--- web-component/src/assets/js/main.js
+++ web-component/src/assets/js/main.js
@@ -1,5 +1,7 @@
 import LigBtn from './components/btn';
 import LigSingleBiography from './components/single/biography';
+import GithubReposWidget from './components/github-repos-widget';
 
 customElements.define('lig-single-biography', LigSingleBiography);
 customElements.define('lig-btn', LigBtn);
+customElements.define('github-repos-widget', GithubReposWidget);
