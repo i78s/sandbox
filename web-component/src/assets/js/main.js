d1200eeda41229a97f2b3de72d43ff1db9d1d1b7
稲葉 聡 <inaba_satoshi@liginc.co.jp>
2017-11-13 23:25:27 +0900

    Custom Elementsのライフサイクルフックの挙動を確認するデモを追加

--- web-component/src/assets/js/main.js
+++ web-component/src/assets/js/main.js
@@ -1,7 +1,9 @@
 import LigBtn from './components/btn';
 import LigSingleBiography from './components/single/biography';
 import GithubReposWidget from './components/github-repos-widget';
+import RadiusGenerator from './components/radius-generator';
 
 customElements.define('lig-single-biography', LigSingleBiography);
 customElements.define('lig-btn', LigBtn);
 customElements.define('github-repos-widget', GithubReposWidget);
+customElements.define('radius-generator', RadiusGenerator);
